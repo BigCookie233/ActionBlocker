78ef7271b343df3b348369212d47a1b598e116ef
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2025-05-05 16:41:42 +0800

    Initial commit

---
+++ src/main/java/io/github/bigcookie233/blocker/ActionBlocker.java
@@ -0,0 +1,10 @@
+package io.github.bigcookie233.blocker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ActionBlocker extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
